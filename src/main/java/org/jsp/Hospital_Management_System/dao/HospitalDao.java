a97157b8b7b62e6bd12606c16740d18222e0d439
SUDHEER13832 <sudheer13832@gmail.com>
2024-03-02 11:30:03 +0530

    added

--- src/main/java/org/jsp/Hospital_Management_System/dao/HospitalDao.java
+++ src/main/java/org/jsp/Hospital_Management_System/dao/HospitalDao.java
@@ -1,5 +1,47 @@
 package org.jsp.Hospital_Management_System.dao;
 
-public class HospitalDao {
+import java.util.List;
+import java.util.Optional;
 
+import org.jsp.Hospital_Management_System.Repository.HospitalRepository;
+import org.jsp.Hospital_Management_System.dto.Hospital;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HospitalDao {
+	@Autowired
+	private HospitalRepository hospitalrepository;
+	
+	public Hospital saveHospital(Hospital hospital) {
+		return hospitalrepository.save(hospital);
+	}
+	
+	
+	
+	public Optional<Hospital> findById(int id) {
+		return hospitalrepository.findById(id);
+	}
+	
+	public boolean deleteById(int id) {
+		Optional<Hospital> rechospital= findById(id);
+		if(rechospital.isPresent()) {
+			hospitalrepository.delete(rechospital.get());
+			return true;
+		}
+		return false;
+	}
+	
+	public Optional<Hospital> findByGst(String gst_number) {
+		return hospitalrepository.findBygstNumber(gst_number);
+	}
+	
+	public Optional<Hospital> findByFounder(String Founder) {
+		return hospitalrepository.findByFounder(Founder);
+	}
+	
+	public List<Hospital> findAll(){
+		return hospitalrepository.findAll();
+	}
 }
+
